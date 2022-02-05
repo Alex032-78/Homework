93b18baf9a0b42e6f6ca23e5bfb5070914fc297b
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-06 01:01:59 +0300

    задача 1

---
+++ Tasks/001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести квадрат числа
+
+Console.Clear();
+double a, sqw=0;
+string s;
+Console.Write("Введите целое число до которого необходимо подсчитать сумму чётных чисел а=");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+
+sqw=a*a;
+System.Console.WriteLine("Квадрат заданного числа: {0}", sqw);
+
+
