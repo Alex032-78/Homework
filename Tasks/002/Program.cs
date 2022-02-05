7c4198198deb7418459f8fd4ffaf1f1c7f0720e6
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-06 01:14:54 +0300

    задача 2

--- Tasks/002/Program.cs
+++ Tasks/002/Program.cs
@@ -6,4 +6,15 @@ string s;
 Console.Write("Введите первое число, которое может оказаться квадратом второго а=");
 s=Console.ReadLine();
 a=Convert.ToDouble(s);
+Console.Write("Введите второе число b=");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
 
+if (a==b*b) 
+{
+    System.Console.WriteLine("{0} квадрат {1}", a, b);
+}
+else
+{
+    System.Console.WriteLine("{0} не является квадратом {1}", a, b);
+}
