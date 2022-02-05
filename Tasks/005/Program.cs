4dd3e80b879bf3a7d632f89d9d090b5f8fe98d2d
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-06 01:43:51 +0300

    задача 5

---
+++ Tasks/005/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Clear();
+double a=0, b, c, max=a;
+string s;
+Console.Write("Введите первое число а=");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+Console.Write("Введите второе число b=");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+Console.Write("Введите третье число c=");
+s=Console.ReadLine();
+c=Convert.ToDouble(s);
+
+if (max<b)
+{
+    max=b;
+}
+if (max<c)
+{
+    max=c;
+}
+if (a==b & a==c)
+{
+    System.Console.WriteLine("Все три числа равны между собой");
+}
+System.Console.WriteLine("Максимальное число {0}", max);
